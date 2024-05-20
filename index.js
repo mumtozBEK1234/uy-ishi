7cfa6e2ff9537736444f383c401c9bbdb8e66237
mumtozBEK1234 <email manzilingiz>
2024-05-20 15:00:53 +0300

    changes

---
+++ index.js
@@ -0,0 +1,108 @@
+// 1
+
+// let m = sm / 100;
+// m = parseInt(m);
+// let sm = + prompt("uzunlik");// console.log(m); 
+
+
+// 2
+// let kilo = + prompt("kilogramni yozing");
+// let tonna = kilo / 1000;
+// tonna = parseInt(tonna);
+// document.write (tonna);
+
+
+// 3
+
+// let bayt = + prompt("baytlarda yozing");
+// let kb = bayt / 1024 ;
+// kb = parseInt(kb);
+// document.write (kb);
+
+// 4
+
+// let a = + prompt ("a kesma");
+// let b = + prompt ("b kesma");
+// let c = a /b ;
+// c = parseInt(c);
+// document.write (c);
+
+
+// 6
+// let a = + prompt ("2honali son yozing");
+// let b = a / 10;
+// b = parseInt(b);
+// let c = a % 10;
+// c = parseInt(c)
+// document.write(b);
+// document.write(c);
+
+
+// 7
+// let a = + prompt ("2 xonali son");
+// let b = a / 10;
+// let c = a % 10;
+// b = parseInt(b);
+// document.write (b+c);
+
+
+// 8
+// let a = + prompt ("2 xonali son");
+// let b = a / 10;
+// let c = a % 10;
+// b = parseInt(b);
+// document.write (c);
+// document.write (b);
+
+
+// 9
+// let son = + prompt ("3honali son yozing");
+// let b = son / 100;
+// b = parseInt(b);
+// document.write(b);
+
+// 10
+// let son = + prompt ("3 honali son yozing");
+// let b = son % 100 ;
+// b = parseInt(b); 
+// let c = b % 10 ;
+// c = parseInt(c)
+// let d = b / 10 ;
+// d = parseInt(d);
+// document.write(c);
+// document.write(d);
+
+// 11
+// let son = + prompt ("3 honali son yozing")
+// let b = son % 10;
+// let c = son % 100;
+// let d = c / 10;
+// d = parseInt(d);
+// let e = son / 100;
+// e = parseInt(e);
+// document.write(b+d+e);
+
+// 12
+// let son = + prompt ("3 honali son yozing")
+// let b = son % 10;
+// let c = son % 100;
+// let d = c / 10;
+// d = parseInt(d);
+// let e = son / 100;
+// e = parseInt(e);
+// document.write(b);
+// document.write(d);
+// document.write(e);
+
+// 13
+// let son = + prompt ("3 honali son yozing")
+// let b = son % 10;
+// let c = son % 100;
+// let d = c / 10;
+// d = parseInt(d);
+// let e = son / 100;
+// e = parseInt(e);
+// document.write(d);
+// document.write(b);
+// document.write(e);
+
